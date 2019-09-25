1bafd02c8df9a3e523e034171036a005a879f941
Brian Simonton <bsimonton78@gmail.com>
2019-09-25 14:54:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,2 @@
-function preventRefreshOnSubmit(){
-    document.querySelector('form').addEventListener('submit', function(event){
-      event.preventDefault()
-    })
-}
 
-preventRefreshOnSubmit()
+
